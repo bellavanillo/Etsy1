e72466aaefa04ac46404a3a7583be6d4ebc9b864
bv <bellavanillo@gmail.com>
2018-05-10 11:07:01 -0700

    add import tags

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { routing } from './app.routing';
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 
